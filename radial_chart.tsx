3e667c2e372568bd127666291e3a1607912e94fa
AANAB1611 <snhussain1610@gmail.com>
2025-02-15 14:41:06 +0530

    Update radial_chart.tsx

--- radial_chart.tsx
+++ radial_chart.tsx
@@ -5,37 +5,38 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-import { formatCurrency } from "@/lib/utils";
-
-const COLORS = ["#0062FF", "#12c6ff", "#ff647f", "#ff9354"];
+const COLORS = [
+  "#0062FF",
+  "#12c6ff",
+  "#ff647f",
+  "#ff9354",
+  "#22c55e",
+  "#f59e0b",
+];
 
 const RadialVariant = ({ data }) => {
   return (
     <ResponsiveContainer width="100%" height={350}>
       <RadialBarChart
         cx="50%"
-        cy="30%"
+        cy="50%"
+        innerRadius="30%"
+        outerRadius="90%"
         barSize={10}
-        innerRadius="90%"
-        outerRadius="40%"
         data={data.map((item, index) => ({
           ...item,
           fill: COLORS[index % COLORS.length],
         }))}
       >
         <RadialBar
-          label={{
-            position: "insideStart",
-            fill: "#fff",
-            fontSize: "12px",
-          }}
+          label={{ position: "insideStart", fill: "#fff", fontSize: "12px" }}
           background
           dataKey="value"
         />
         <Legend
           layout="horizontal"
           verticalAlign="bottom"
-          align="right"
+          align="center"
           iconType="circle"
           content={(props) => {
             const { payload } = props;
@@ -55,9 +56,7 @@ const RadialVariant = ({ data }) => {
                       <span className="text-sm text-muted-foreground">
                         {entry.value}
                       </span>
-                      <span className="text-sm">
-                        {formatCurrency(entry.payload?.value)}
-                      </span>
+                      <span className="text-sm">{entry.payload?.value}</span>
                     </div>
                   </li>
                 ))}
