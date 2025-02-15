c1092084e69879cda19f43747dac3ef01c301c6d
AANAB1611 <snhussain1610@gmail.com>
2025-02-15 10:49:44 +0530

    Create bar_chart.tsx

---
+++ bar_chart.tsx
@@ -0,0 +1,34 @@
+import { format } from "date-fns";
+import {
+  BarChart,
+  XAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+  Bar,
+  Tooltip,
+} from "recharts";
+
+import { CustomTooltip } from "./custom-tooltip";
+
+const BarVariant = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          axisLine={false}
+          tickLine={false}
+          dataKey="date"
+          tickFormatter={(value) => format(new Date(value), "dd MMM")}
+          style={{ fontSize: "12px" }}
+          tickMargin={16}
+        />
+        <Tooltip content={<CustomTooltip />} />
+        <Bar dataKey="income" fill="#3b82f6" className="drop-shadow-sm" />
+        <Bar dataKey="expenses" fill="#f43f5e" className="drop-shadow-sm" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default BarVariant;
